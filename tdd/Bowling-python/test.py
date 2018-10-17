6874b6924e79029a4f5caf0cff7599b66cdb4e38
Suhyun Kim <ideptor@gmail.com>
2018-10-18 01:49:45 +0900

    refactoring test.py

--- tdd/Bowling-python/test.py
+++ tdd/Bowling-python/test.py
@@ -2,14 +2,16 @@ from bowling import Bowling
 
 def test_zeros():
     game = Bowling()
-    for i in range(0,20):
-        game.roll(0)
+    repeat_rolls(game,0,20)
 
     assert game.score() == 0
 
 def test_ones():
     game = Bowling()
-    for i in range(0,20):
-        game.roll(1)
+    repeat_rolls(game,1,20)
     
     assert game.score() == 20
+
+def repeat_rolls(game, pin, repeat_num):
+    for i in range(0, repeat_num):
+        game.roll(pin)
